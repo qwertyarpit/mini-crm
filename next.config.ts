4b8595d99e989d44f26b6a6f552ede63c79cdb18
qwertyarpit <75834909+qwertyarpit@users.noreply.github.com>
2024-11-19 02:16:02 +0530

    new

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
